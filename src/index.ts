71020f37e6e21d3923ca71760f6ba801e495e4d5
Ashutosh Mulky <44554225+Ash9311@users.noreply.github.com>
2024-03-09 23:55:12 +0530

    blog,user logic

--- src/index.ts
+++ src/index.ts
@@ -3,44 +3,23 @@ import { Hono } from 'hono'
 
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
+import { sign } from 'hono/jwt'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
 
 const app = new Hono<{
   Bindings: {
-    DATABASE_URL: string
+    DATABASE_URL: string,
+    JWT_SECRET: string
   }
 }>()
 
-app.post('/api/v1/user/signup', (c) => {
-  const prisma = new PrismaClient({
-    datasourceUrl: c.env.DATABASE_URL,
-  }).$extends(withAccelerate())
+app.route("/api/v1/user", userRouter)
+app.route("/api/v1/blog", blogRouter)
 
-  return c.text('Hello Hono!')
-})
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
-})
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
 
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
-
-app.get('/api/v1/blog/blog', (c) => {
-  return c.text('Hello Hono!')
-})
 
 
 export default app
